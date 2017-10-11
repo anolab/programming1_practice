c96ab571d805f7cd7af5d69aa05deb6c7c78cd74
lifesailor <sailyourlife91@gmail.com>
2017-10-11 16:57:06 +0900

    4. Study Class 생성

---
+++ git_practice/git_practice/Study.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_practice
+{
+    class Study
+    {
+        private int kind;
+
+        public int Kind { get; set; }
+    }
+}
