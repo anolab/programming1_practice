b16623d02e5bb0ffba98650e17b21b4cae2f5577
lifesailor <sailyourlife91@gmail.com>
2017-10-11 16:48:37 +0900

    2. Person Class 생성

---
+++ git_practice/git_practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_practice
+{
+    class Person
+    {
+        private string name;
+        private int grade;
+
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
